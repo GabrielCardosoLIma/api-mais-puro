7457320ebfa2bf8aa5c73cb0dd2efe2aabf9f5b3
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2022-06-14 22:08:12 -0300

    api  maps

---
+++ script.js
@@ -0,0 +1,42 @@
+// let h2 = document.querySelector('h2');
+var map = L.map('mapid').setView([-22.8992832,-47.0612541,75], 13);
+
+// function success(pos){
+//     console.log(pos.coords.latitude, pos.coords.longitude);
+//     h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+
+//     if (map === undefined) {
+//         map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+//     } else {
+//         map.remove();
+//         map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
+//     }
+
+//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
+
+//     L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+//         .bindPopup('Eu estou aqui!')
+//         .openPopup();
+// }
+
+// function error(err){
+//     console.log(err);
+// }
+
+// var watchID = navigator.geolocation.watchPosition(success, error, {
+//     enableHighAccuracy: true,
+//     timeout: 5000
+// });
+
+//navigator.geolocation.clearWatch(watchID);
+
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([-22.8992832,-47.0612541,75]).addTo(map)
+        .bindPopup('Você está aqui')
+        .openPopup();
